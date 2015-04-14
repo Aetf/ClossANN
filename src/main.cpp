88e57563e4cc5bea8d546c9e0350c47381384ad5
Aetf <7437103@gmail.com>
2015-04-14 10:54:41 +0800

    some renaming

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,31 @@ using namespace OpenANN;
 int main()
 {
   // Create dataset
-  const int D = 2; // number of inputs
-  const int F = 1; // number of outputs
-  const int N = 4; // size of training set
-  Eigen::MatrixXd X(N, D); // inputs
-  Eigen::MatrixXd T(N, F); // desired outputs (targets)
+  const int nI = 2; // number of inputs
+  const int nO = 1; // number of outputs
+  const int nP = 4; // size of training set
+  Eigen::MatrixXd P(nP, nI); // inputs
+  Eigen::MatrixXd T(nP, nO); // desired outputs (targets)
   // Each row represents an instance
-  X.row(0) << 0.0, 1.0;
+  P.row(0) << 0.0, 1.0;
   T.row(0) << 1.0;
-  X.row(1) << 0.0, 0.0;
+  P.row(1) << 0.0, 0.0;
   T.row(1) << 0.0;
-  X.row(2) << 1.0, 1.0;
+  P.row(2) << 1.0, 1.0;
   T.row(2) << 0.0;
-  X.row(3) << 1.0, 0.0;
+  P.row(3) << 1.0, 0.0;
   T.row(3) << 1.0;
-  DirectStorageDataSet dataSet(&X, &T);
+  DirectStorageDataSet dataSet(&P, &T);
 
   // Make the result repeatable
   RandomNumberGenerator().seed(0);
 
   // Create network
   Net net;
-  // Add an input layer with D inputs, 1 hidden layer with 2 nodes and an
-  // output layer with F outputs. Use logistic activation function in hidden
+  // Add an input layer with nI inputs, 1 hidden layer with 2 nodes and an
+  // output layer with nO outputs. Use logistic activation function in hidden
   // layer and output layer.
-  makeMLNN(net, LOGISTIC, LOGISTIC, D, F, 1, 2);
+  makeMLNN(net, LOGISTIC, LOGISTIC, nI, nO, 1, 2);
   // Add training set
   net.trainingSet(dataSet);
 
@@ -47,7 +47,7 @@ int main()
   train(net, "LMA", MSE, stop);
 
   // Use network to predict labels of the training data
-  for(int n = 0; n < N; n++)
+  for(int n = 0; n < nP; n++)
   {
     Eigen::VectorXd y = net(dataSet.getInstance(n));
     std::cout << y << std::endl;
