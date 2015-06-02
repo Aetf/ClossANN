Aetf/ClossANN

commit a8aa390044f7a0db5f7ef863a4d05f5d14286744

Clean up code

app/eyecandy/widgets/mainwindow.cpp
@@ -49,7 +49,7 @@ MainWindow::MainWindow(QWidget *parent)
     // for testing
     ui->tabs->setUpdatesEnabled(false);
     for (int i = 1; i!= 8; i++) {
-        if (i == 4) continue;
+//        if (i == 4) continue;
         auto plot = new QCustomPlot;
         setupDemo(i, plot);
         ui->tabs->addTab(plot, demoName);
@@ -80,26 +80,26 @@ void MainWindow::setupOptionPage()
     // Group Net
     ui->btnRefreshSeed->setIcon(AwesomeIconProvider::instance()->icon(fa::refresh));
     connect(ui->spinLearnRate, Select<double>::OverloadOf(&QDoubleSpinBox::valueChanged),
-    this, [=](auto value) {
-        this->currentParam.learningRate(value);
+    this, [&](auto value) {
+        currentParam.learningRate(value);
     });
     connect(ui->lineRandSeed, &QLineEdit::textChanged,
     [&](auto text) {
         currentParam.randSeed(text.toUInt());
     });
     connect(ui->btnRefreshSeed, &QAbstractButton::clicked,
-    this, [=] {
+    this, [&] {
         ui->lineRandSeed->setText(QString::number(get_seed()));
     });
 
     // Group Closs
     connect(ui->spinKernelSize, Select<double>::OverloadOf(&QDoubleSpinBox::valueChanged),
-    this, [=](auto value) {
-        this->currentParam.kernelSize(value);
+    this, [&](auto value) {
+        currentParam.kernelSize(value);
     });
     connect(ui->spinPValue, Select<double>::OverloadOf(&QDoubleSpinBox::valueChanged),
-    this, [=](auto value) {
-        this->currentParam.pValue(value);
+    this, [&](auto value) {
+        currentParam.pValue(value);
     });
 
     // Group Data
@@ -140,13 +140,13 @@ void MainWindow::setupOptionPage()
             this, &MainWindow::updateButtons);
 
     connect(ui->btnInsertLayer, &QAbstractButton::clicked,
-    this, [=] {
-        auto pos = this->ui->tableNetStru->selectionModel()->currentIndex().row();
+    this, [&] {
+        auto pos = ui->tableNetStru->selectionModel()->currentIndex().row();
         layersModel->insertLayer(pos + 1, LayerDesc());
     });
     connect(ui->btnRemoveLayer, &QAbstractButton::clicked,
-    this, [=] {
-        auto pos = this->ui->tableNetStru->selectionModel()->currentIndex().row();
+    this, [&] {
+        auto pos = ui->tableNetStru->selectionModel()->currentIndex().row();
         layersModel->removeRows(pos, 1);
     });
 
@@ -211,7 +211,7 @@ void MainWindow::applyOptions()
 
 void MainWindow::displayDefaultOptions()
 {
-    LearnParam param;
+    LearnParam& param = currentParam;
     // Group Net
     ui->spinLearnRate->setValue(param.learningRate());
     ui->lineRandSeed->setText(QString::number(param.randSeed()));
@@ -865,8 +865,6 @@ void MainWindow::bracketDataSlot()
 
 void MainWindow::setupMonitorPage()
 {
-    demoName = "Closs ANN";
-
     setupProblemPlane(ui->plotColorMap);
     setupErrorLine(ui->plotErrorLine);
 }
@@ -904,7 +902,7 @@ QCPColorMap *MainWindow::createPredictMap(QCPAxis *xAxis, QCPAxis *yAxis, QCPCol
 
     // connect to data change signal
     connect(handler.get(), &UIHandler::predictionUpdated,
-    [=](auto data) {
+    [&](auto data) {
         for (auto item : data) {
             auto point = item.toList();
             int x, y;
@@ -1030,7 +1028,7 @@ void MainWindow::setupProblemPlane(QCustomPlot *plot)
 
     // postpone two scatters setup to after applying options
     connect(handler.get(), &UIHandler::inputRangeUpdated,
-    this, [=](auto min, auto max) {
+    this, [&](auto min, auto max) {
         QCPRange range(min, max);
         predictMap->data()->setRange(range, range);
         plot->xAxis->setRange(range);
@@ -1038,7 +1036,7 @@ void MainWindow::setupProblemPlane(QCustomPlot *plot)
     });
 
     connect(handler.get(), &UIHandler::outputRangeUpdated,
-    this, [=](auto min, auto max, auto labelsCount) {
+    this, [&](auto min, auto max, auto labelsCount) {
         // set prediction map data range
         scale->setDataRange(QCPRange(min, max));
 
@@ -1051,6 +1049,10 @@ void MainWindow::setupProblemPlane(QCustomPlot *plot)
 
 void MainWindow::setupErrorLine(QCustomPlot *plot)
 {
+    plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+    plot->legend->setVisible(true);
+    plot->setAutoAddPlottableToLegend(true);
+
     auto graphTrain = plot->addGraph();
     graphTrain->setPen(QPen(Qt::blue));
     graphTrain->setBrush(QBrush(QColor(150, 222, 0)));
@@ -1067,9 +1069,7 @@ void MainWindow::setupErrorLine(QCustomPlot *plot)
 
     // connect to relative signals
     connect(handler.get(), &UIHandler::iterationFinished,
-            this,
-            [graphTrain, graphTesting, plot](auto, auto iter,
-                                             auto error, auto testError){
+    this, [&](auto, auto iter, auto error, auto testError) {
         // training error
         graphTrain->addData(iter, error);
         graphTesting->addData(iter, testError);
@@ -1084,7 +1084,7 @@ void MainWindow::setupErrorLine(QCustomPlot *plot)
         plot->replot();
     });
     connect(handler.get(), &UIHandler::trainingStopped,
-            this, [graphTrain, graphTesting]{
+    this, [&] {
         graphTrain->clearData();
         graphTesting->clearData();
     });

app/eyecandy/logic/uihandler.cpp
@@ -106,7 +106,7 @@ void UIHandler::run()
     opt.setStopCriteria(task->stopCriteria());
 
     // protect multithread access to data
-    auto step = [&](){
+    auto step = [&]() {
         // ensure data is in training mode
         auto ctx = task->data().enterTrainingMode(false);
         return opt.step();
@@ -115,8 +115,8 @@ void UIHandler::run()
     while(step())
     {
         Logger::info() << "第" << opt.currentIteration() << "次迭代，"
-                      << "Error = "
-                      << QStringLiteral("%1").arg(opt.currentError(), 0, 'g', 4);
+                       << "Error = "
+                       << QStringLiteral("%1").arg(opt.currentError(), 0, 'g', 4);
 
         double testError = 0.0;
         // compute testing error, ensure in testing mode

app/eyecandy/models/layerdescmodel.cpp
@@ -167,9 +167,11 @@ QVariant LayerDescModel::dataForColActFunc(int row, int role) const
 }
 
 QVariant LayerDescModel::headerData(int section, Qt::Orientation orientation,
-                                       int role) const
+                                    int role) const
 {
-    if (role != Qt::DisplayRole) { return QVariant(); }
+    if (role != Qt::DisplayRole) {
+        return QVariant();
+    }
 
     if (orientation != Qt::Horizontal) {
         return section + 1;
@@ -191,13 +193,17 @@ QVariant LayerDescModel::headerData(int section, Qt::Orientation orientation,
 
 int LayerDescModel::rowCount(const QModelIndex &parent) const
 {
-    if (parent.isValid()) { return 0; }
+    if (parent.isValid()) {
+        return 0;
+    }
     return layers_.size();
 }
 
 int LayerDescModel::columnCount(const QModelIndex &parent) const
 {
-    if (parent.isValid()) { return 0; }
+    if (parent.isValid()) {
+        return 0;
+    }
     return ColCount;
 }
 
@@ -282,8 +288,8 @@ bool LayerDescModel::removeRows(int row, int count, const QModelIndex &parent)
 bool LayerDescModel::inRange(const QModelIndex &index) const
 {
     return index.isValid()
-        && index.row() >= 0 && index.row() < layers_.size()
-        && index.column() >= 0 && index.column() < ColCount;
+           && index.row() >= 0 && index.row() < layers_.size()
+           && index.column() >= 0 && index.column() < ColCount;
 }
 
 void LayerDescModel::clear()

app/eyecandy/utils/logger.cpp
@@ -21,14 +21,14 @@ Q_GLOBAL_STATIC(LoggerStaticInitializer, loggerStaticInitializer)
 
 namespace Log
 {
-    Msg::Msg() {}
+Msg::Msg() {}
 
-    Msg::Msg(int id, MsgType type, const QString &message)
-        : id(id)
-        , timestamp(QDateTime::currentMSecsSinceEpoch())
-        , type(type)
-        , message(message)
-    { }
+Msg::Msg(int id, MsgType type, const QString &message)
+    : id(id)
+    , timestamp(QDateTime::currentMSecsSinceEpoch())
+    , type(type)
+    , message(message)
+{ }
 
 }
 

app/eyecandy/utils/logger.h
@@ -11,23 +11,23 @@ const int MAX_LOG_MESSAGES = 1000;
 class Logger;
 namespace Log
 {
-    enum MsgType
-    {
-        INFO,
-        NORMAL,
-        WARNING,
-        CRITICAL //ERROR is defined by libtorrent and results in compiler error
-    };
+enum MsgType
+{
+    INFO,
+    NORMAL,
+    WARNING,
+    CRITICAL //ERROR is defined by libtorrent and results in compiler error
+};
 
-    struct Msg
-    {
-        Msg();
-        Msg(int id, MsgType type, const QString &message);
-        int id;
-        qint64 timestamp;
-        MsgType type;
-        QString message;
-    };
+struct Msg
+{
+    Msg();
+    Msg(int id, MsgType type, const QString &message);
+    int id;
+    qint64 timestamp;
+    MsgType type;
+    QString message;
+};
 }
 Q_DECLARE_METATYPE(Log::MsgType)
 Q_DECLARE_METATYPE(Log::Msg)

app/eyecandy/utils/utils.h
@@ -2,6 +2,8 @@
 #define UTILS_H
 
 #include <memory>
+#include <QMetaObject>
+#include <QObject>
 using std::unique_ptr;
 
 class Utils
@@ -16,7 +18,8 @@ unique_ptr<T> make_unique(T *pointer)
     return unique_ptr<T>(pointer);
 }
 
-template<typename... Args> struct Select {
+template<typename... Args> struct Select
+{
     template<typename C, typename R>
     static constexpr auto OverloadOf( R (C::*pmf)(Args...) ) -> decltype(pmf) {
         return pmf;

commit be3b87b3c768e903533a22a5e1f178bd14cd4941

Fix crush caused by incorrect capture-by-reference usage introduced in a8aa390

app/eyecandy/widgets/mainwindow.cpp
@@ -80,31 +80,31 @@ void MainWindow::setupOptionPage()
     // Group Net
     ui->btnRefreshSeed->setIcon(AwesomeIconProvider::instance()->icon(fa::refresh));
     connect(ui->spinLearnRate, Select<double>::OverloadOf(&QDoubleSpinBox::valueChanged),
-    this, [&](auto value) {
+    this, [=](auto value) {
         currentParam.learningRate(value);
     });
     connect(ui->lineRandSeed, &QLineEdit::textChanged,
-    [&](auto text) {
+    [=](auto text) {
         currentParam.randSeed(text.toUInt());
     });
     connect(ui->btnRefreshSeed, &QAbstractButton::clicked,
-    this, [&] {
+    this, [=] {
         ui->lineRandSeed->setText(QString::number(get_seed()));
     });
 
     // Group Closs
     connect(ui->spinKernelSize, Select<double>::OverloadOf(&QDoubleSpinBox::valueChanged),
-    this, [&](auto value) {
+    this, [=](auto value) {
         currentParam.kernelSize(value);
     });
     connect(ui->spinPValue, Select<double>::OverloadOf(&QDoubleSpinBox::valueChanged),
-    this, [&](auto value) {
+    this, [=](auto value) {
         currentParam.pValue(value);
     });
 
     // Group Data
     connect(ui->comboDataSource, Select<int>::OverloadOf(&QComboBox::currentIndexChanged),
-    this, [&]() {
+    this, [=]() {
         auto source = DataSource(ui->comboDataSource->currentData().toInt());
         currentParam.dataSource(source);
 
@@ -116,11 +116,11 @@ void MainWindow::setupOptionPage()
     ui->comboDataSource->addItem("CSV", DataSource::CSV);
 
     connect(ui->lineCSVFile, &QLineEdit::textChanged,
-    this, [&](auto text) {
+    this, [=](auto text) {
         currentParam.csvFilePath(text);
     });
     connect(ui->btnCSVBrowse, &QAbstractButton::clicked,
-    this, [&]() {
+    this, [=]() {
         auto path = QFileDialog::getOpenFileName(this, tr("打开CSV数据文件"),
                     "",
                     tr("CSV 文件(*.csv)"));
@@ -140,12 +140,12 @@ void MainWindow::setupOptionPage()
             this, &MainWindow::updateButtons);
 
     connect(ui->btnInsertLayer, &QAbstractButton::clicked,
-    this, [&] {
+    this, [=] {
         auto pos = ui->tableNetStru->selectionModel()->currentIndex().row();
         layersModel->insertLayer(pos + 1, LayerDesc());
     });
     connect(ui->btnRemoveLayer, &QAbstractButton::clicked,
-    this, [&] {
+    this, [=] {
         auto pos = ui->tableNetStru->selectionModel()->currentIndex().row();
         layersModel->removeRows(pos, 1);
     });
@@ -205,7 +205,7 @@ void MainWindow::applyOptions()
     param.layers(layersModel->layers());
 
     currentParam = param;
-    Logger::normal("初始化网络");
+    Logger::normal("初始化\n网络");
     handler->configure(param);
 }
 
@@ -902,7 +902,7 @@ QCPColorMap *MainWindow::createPredictMap(QCPAxis *xAxis, QCPAxis *yAxis, QCPCol
 
     // connect to data change signal
     connect(handler.get(), &UIHandler::predictionUpdated,
-    [&](auto data) {
+    [=](auto data) {
         for (auto item : data) {
             auto point = item.toList();
             int x, y;
@@ -1028,7 +1028,7 @@ void MainWindow::setupProblemPlane(QCustomPlot *plot)
 
     // postpone two scatters setup to after applying options
     connect(handler.get(), &UIHandler::inputRangeUpdated,
-    this, [&](auto min, auto max) {
+    this, [=](auto min, auto max) {
         QCPRange range(min, max);
         predictMap->data()->setRange(range, range);
         plot->xAxis->setRange(range);
@@ -1036,7 +1036,7 @@ void MainWindow::setupProblemPlane(QCustomPlot *plot)
     });
 
     connect(handler.get(), &UIHandler::outputRangeUpdated,
-    this, [&](auto min, auto max, auto labelsCount) {
+    this, [=](auto min, auto max, auto labelsCount) {
         // set prediction map data range
         scale->setDataRange(QCPRange(min, max));
 
@@ -1069,7 +1069,7 @@ void MainWindow::setupErrorLine(QCustomPlot *plot)
 
     // connect to relative signals
     connect(handler.get(), &UIHandler::iterationFinished,
-    this, [&](auto, auto iter, auto error, auto testError) {
+    this, [=](auto, auto iter, auto error, auto testError) {
         // training error
         graphTrain->addData(iter, error);
         graphTesting->addData(iter, testError);
@@ -1084,7 +1084,7 @@ void MainWindow::setupErrorLine(QCustomPlot *plot)
         plot->replot();
     });
     connect(handler.get(), &UIHandler::trainingStopped,
-    this, [&] {
+    this, [=]{
         graphTrain->clearData();
         graphTesting->clearData();
     });